af749e92ad5e5f908ca085cb5ed3a72f37c17dba
TUHINPRO <71925866+TUHINPRO@users.noreply.github.com>
2021-06-28 17:55:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function move_to_new_location(){
+    window.location="bedroom.html";
+}
+function move_to_new_location2() {
+    window.location="kitchen.html"
+}
+function move_to_new_location3() {
+    window.location="mall.html"
+}
